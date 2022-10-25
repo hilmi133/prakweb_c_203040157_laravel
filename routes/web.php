29437dcd1a31d5320908d4d2165b82f4af85976e
unknown <hilmibadruz685@gmail.com>
2022-10-25 11:58:03 +0700

    struktur folder, routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home');
+});
+
+Route::get('/about', function () {
+    return view ('about',[
+        "name" => "Hilmi Badruzaman",
+        "email" => "hilmibadruzaman@gmail.com",
+        "image" => "profile.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view ('posts');
 });
