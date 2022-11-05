dee907811527aa614678ffa7c5e9614c05800f09
unknown <hilmibadruz685@gmail.com>
2022-11-05 20:44:34 +0700

    View login Registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\User;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,3 +45,5 @@ Route::get('/categories', function(){
         'categories' => Category::all()
     ]);
 });
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
